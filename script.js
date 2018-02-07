e0957a7b9e9092f5ef81b82b88454fcf9cc1f1bf
meloncatty <k.handel66@gmail.com>
2018-02-07 11:22:46 -0700

    initial commit

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
-$(function () {
-  $('.web-title, .web-url').on('keyup', function () {
-    if ($('.web-title').val() === '' || $('.web-url').val() === '') {
-      $('.submit-btn').prop('disabled', true);
-    } else {
-      $('.submit-btn').prop('disabled', false);
+$(document).ready(function(){
+  $(document).on('submit', function(e) {
+    if($('form').val() === '' && e.which === 13) {
+      alert('enter valid title and url')
     }
   });
-});
-$('.submit-btn').on('keypress click', function(event) {
-  if(event.which === 13 || event.type === 'click'){
-    event.preventDefault();
-    var webTitle = $('.web-title').val();
-    var webUrl = $('.web-url').val();
-    if($('.web-title').val() === ' ' || $('.web-url').val() === ' '){
-      $('input[type=`text`].web-title').text('Enter a valid title');
-      $('input[type=`text`].web-url').text('Enter a valid URL');
-    }
-    $('ul').append(`<li><h2> ${webTitle} </h2><hr><h3><a href='https://${webUrl}'>${webUrl}</a></h3><hr>
-    <button type ='submit' class ="mark-as-read">Read</button><button type ='submit' class ="delete">Delete</button></li>`);
+  $(function () {
+    $('.web-title, .web-url').on('keyup', function () {
+      if ($('.web-title').val() === '' && $('.web-url').val() === '') {
+        $('.submit-btn').prop('disabled', true);
+      } else {
+        $('.submit-btn').prop('disabled', false);
+      }
+    });
+  });
+  function is_valid_url(url) {
+    return /^(http(s)?:\/\/)?(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/.test(url);
   }
-  $('.delete').hover(function(){
-    $(this).toggleClass('card-btn');
-  })
-  $('.mark-as-read').hover(function() {
-    $(this).toggleClass('card-btn');
-  })
-  $('.mark-as-read').click(function(){
-    $(this).toggleClass('read')
-  })
-  $('li').on('click', '.delete', function() {
-    $(this).parent().remove();
+  $('.submit-btn').on('keypress click', function(event) {
+    var count = 1 + $('li').length;
+    if(event.which === 13 || event.type === 'click'){
+      event.preventDefault();
+      var webTitle = $('.web-title').val();
+      var webUrl = $('.web-url').val();
+      if(!is_valid_url(webUrl)){
+        alert('invalid url')
+      } else if ($('.web-title').val() === ''){
+        alert('enter a valid title');
+      } else {
+        $('.total-bookmarks').text(count)
+        $('ul').append(`<li><h2> ${webTitle} </h2><hr><h3><a href='https://${webUrl}'>${webUrl}</a></h3><hr>
+        <button type ='submit' class ="mark-as-read">Read</button><button type ='submit' class ="delete">Delete</button></li>`);
+      }
+      $('form').children('input').val('');
+    }
+    $('ul').on('click', '.mark-as-read', function(){
+      $(this).toggleClass('read')
+    })
+    $('ul').on('click', '.delete', function() {
+      $(this).parent().remove();
+    });
   });
 });
